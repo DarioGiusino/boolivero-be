a063ebf6c5b2c9b65f59d4ad9dcb0123ae8eddc0
DarioGiusino <dario.giusino@gmail.com>
2023-04-03 12:18:41 +0200

    renamed relationships

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -8,11 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     use HasFactory;
-    public function User()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
-    public function Categories()
+    public function categories()
     {
         return $this->belongsToMany(Category::class);
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function Restaurant()
+    public function restaurant()
     {
         return $this->hasOne(Restaurant::class);
     }
