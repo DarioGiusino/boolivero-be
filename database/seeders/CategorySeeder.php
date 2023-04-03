2f334e873e3bda2db1475ac8e77c3dbcebcddf95
DarioGiusino <dario.giusino@gmail.com>
2023-04-03 12:46:27 +0200

    categories seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,40 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            [
+                'label' => 'Italiano',
+                'color' => '#e91a1a',
+                'image' => '',
+            ],
+            [
+                'label' => 'Argentino',
+                'color' => '#1ab7e9',
+                'image' => '',
+            ],
+            [
+                'label' => 'Giapponese',
+                'color' => '#8e0ad8',
+                'image' => '',
+            ],
+            [
+                'label' => 'Messicano',
+                'color' => '#d87a0a',
+                'image' => '',
+            ],
+            [
+                'label' => 'Indiano',
+                'color' => '#43a90d',
+                'image' => '',
+            ],
+        ];
+
+        foreach ($categories as $category) {
+            $new_category = new Category();
+            $new_category->label = $category['label'];
+            $new_category->color = $category['color'];
+            $new_category->image = $category['image'];
+            $new_category->save();
+        }
     }
 }
