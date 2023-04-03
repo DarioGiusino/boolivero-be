6b7e2124bac897d691bd487d7a45646930d17874
DarioGiusino <dario.giusino@gmail.com>
2023-04-03 11:25:18 +0200

    foods table(to do fk)

---
+++ database/migrations/2023_04_03_091307_create_foods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('foods', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+            $table->text('description')->nullable();
+            $table->decimal('price', 6, 2);
+            $table->boolean('is_published')->default(0);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('foods');
+    }
+};
