2b3ebf2c6fc00ef78c814d0c3321d0e5ae0da194
VictorGiuliano <victorgiuliano98@gmail.com>
2023-04-04 11:58:22 +0200

    Add new users and new restaurants associate an User

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -27,9 +27,42 @@ class RestaurantSeeder extends Seeder
 
         $new_restaurant->save();
 
-        $restaurants = [
+        $new_second_resturant = new Restaurant();
+        $new_second_resturant->user_id = $users_id[1];
+        $new_second_resturant->restaurant_name = 'Pizzeria Ciro';
+        $new_second_resturant->address = 'Via Boolean, 88';
+        $new_second_resturant->p_iva = '86334519758';
+        $new_second_resturant->banner = 'https://picsum.photos/536/354';
+        $new_second_resturant->vote = 2.4;
+
+        $new_second_resturant->save();
+
+        $new_third_resturant = new Restaurant();
+        $new_third_resturant->user_id = $users_id[2];
+        $new_third_resturant->restaurant_name = 'Al Gelatone';
+        $new_third_resturant->address = 'Via Boolean, 81';
+        $new_third_resturant->p_iva = '86334519759';
+        $new_third_resturant->banner = 'https://picsum.photos/536/354';
+        $new_third_resturant->vote = 4.4;
+
+        $new_third_resturant->save();
+
+
+        $new_fourth_resturant = new Restaurant();
+        $new_fourth_resturant->user_id = $users_id[3];
+        $new_fourth_resturant->restaurant_name = 'Paninoteca Guzza';
+        $new_fourth_resturant->address = 'Via Boolean, 100';
+        $new_fourth_resturant->p_iva = '86334519753';
+        $new_fourth_resturant->banner = 'https://picsum.photos/536/354';
+        $new_fourth_resturant->vote = 1.2;
+
+        $new_fourth_resturant->save();
+
+        /*$restaurants = [
             [
+                
                 'restaurant_name' => 'Pizzeria Ciro',
+                'user_id' => 1,
                 'address' => 'Via del Baffo, 14',
                 'p_iva' => '86334519751',
                 'banner' => 'https://picsum.photos/536/354',
@@ -37,6 +70,7 @@ class RestaurantSeeder extends Seeder
             ],
             [
                 'restaurant_name' => 'Al Gelatone',
+                'user_id' => 2,
                 'address' => 'Via delle Magnolie, 89',
                 'p_iva' => '86334519752',
                 'banner' => 'https://picsum.photos/536/354',
@@ -44,6 +78,7 @@ class RestaurantSeeder extends Seeder
             ],
             [
                 'restaurant_name' => 'Paninoteca Guzza',
+                'user_id' => 3,
                 'address' => 'Via calzone, 18',
                 'p_iva' => '86334519753',
                 'banner' => 'https://picsum.photos/536/354',
@@ -51,6 +86,7 @@ class RestaurantSeeder extends Seeder
             ],
             [
                 'restaurant_name' => 'Kalos',
+                'user_id' => 0,
                 'address' => 'Via Terrasini, 57',
                 'p_iva' => '86334519754',
                 'banner' => 'https://picsum.photos/536/354',
@@ -58,6 +94,7 @@ class RestaurantSeeder extends Seeder
             ],
             [
                 'restaurant_name' => 'Sushi Wong',
+                'user_id' => 0,
                 'address' => 'Via Yoshimizu, 90',
                 'p_iva' => '86334519755',
                 'banner' => 'https://picsum.photos/536/354',
@@ -65,6 +102,7 @@ class RestaurantSeeder extends Seeder
             ],
             [
                 'restaurant_name' => 'Sakura',
+                'user_id' => 0,
                 'address' => 'Via degli Anime, 33',
                 'p_iva' => '86334519756',
                 'banner' => 'https://picsum.photos/536/354',
@@ -72,22 +110,24 @@ class RestaurantSeeder extends Seeder
             ],
             [
                 'restaurant_name' => 'Da Mindu',
+                'user_id' => 0,
                 'address' => 'Via dei molti nomi, 5',
                 'p_iva' => '86334519758',
                 'banner' => 'https://picsum.photos/536/354',
                 'vote' => '4.7',
             ],
-        ];
+        ];*/
 
-        foreach ($restaurants as $restaurant) {
+        /*foreach ($restaurants as $restaurant) {
             $new_second_resturant = new Restaurant();
             $new_second_resturant->restaurant_name = $restaurant['restaurant_name'];
+            $new_second_resturant->user_id = $users_id['user_id'];
             $new_second_resturant->address = $restaurant['address'];
             $new_second_resturant->p_iva = $restaurant['p_iva'];
             $new_second_resturant->banner = $restaurant['banner'];
             $new_second_resturant->vote = $restaurant['vote'];
 
             $new_second_resturant->save();
-        }
+        }*/
     }
 }
