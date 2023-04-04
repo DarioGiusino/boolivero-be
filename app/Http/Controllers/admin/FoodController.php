413af20376534c8ef3a5fc20a012d8956d61dcfc
DarioGiusino <dario.giusino@gmail.com>
2023-04-04 21:42:11 +0200

    added file update logic + update method in food controller

--- app/Http/Controllers/admin/FoodController.php
+++ app/Http/Controllers/admin/FoodController.php
@@ -63,26 +63,26 @@ class FoodController extends Controller
         $food = new Food();
 
         // todo define slug
-        // $food->slug = Str::slug($data['title'], '-');
+        // $food->slug = Str::slug($data['label'], '-');
 
-        // todo check if an image is given
-        //  if (Arr::exists($data, 'image')) {
-        //      // take the image extension
-        //      $extension = $data['image']->extension();
-        //      // build the image file name with the slug (unique causa depends on title witch is unique) + extension
-        //      $file_name = "$food->slug.$extension";
-        //      // define a variable where the file is saved in a path storage/app/public/{} that return a correct URL
-        //      $img_url = Storage::putFileAs('foods', $data['image'], $file_name);
-        //      // change the file given with the correct url
-        //      $data['image'] = $img_url;
-        //  }
-
+        // check if an image is given
+        if (Arr::exists($data, 'image')) {
+            // todo take the image extension
+            // $extension = $data['image']->extension();
+            // todo build the image file name with the slug (unique causa depends on title witch is unique) + extension
+            // $file_name = "$food->slug.$extension";
+            // $img_url = Storage::putFileAs('foods', $data['image'], $file_name);
+            // define a variable where the file is saved in a path storage/app/public/{} that return a correct URL
+            $img_url = Storage::put('foods', $data['image']);
+            // change the file given with the correct url
+            $data['image'] = $img_url;
+        }
 
         // fill new food with data from form
         $food->fill($data);
 
-        // todo define publish or not
-        // $food->is_published = Arr::exists($data, 'is_published');
+        // define publish or not
+        $food->is_published = Arr::exists($data, 'is_published');
 
         // define the food restaurant as the user logged
         $food->restaurant_id = Auth::id();
@@ -109,9 +109,54 @@ class FoodController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Food $food)
     {
-        //
+        // ! validation
+        // ! validation
+        $request->validate(
+            [
+                'label' => 'required|string|max:40',
+                'description' => 'string',
+                'image' => 'nullable|image|mimes:png,jpeg,jpg',
+                'price' => 'required|numeric'
+            ],
+            [
+                'label.required' => 'Devi inserire un nome.',
+                'label.string' => 'Il nome deve essere alfanumerico.',
+                'label.max' => 'Lunghezza massima superata, max: 40.',
+                'description.string' => 'La descrizione deve essere alfanumerica e non può essere vuota.',
+                'image.image' => 'Inserisci un file di tipo immagine.',
+                'image.mimes' => 'Sono supportati solo jpeg, jpg and png.',
+                'price.required' => 'Devi inserire un prezzo.',
+                'price.numeric' => 'Il prezzo deve essere numerico.'
+            ]
+        );
+
+        $data = $request->all();
+        // todo define slug
+        // $food->slug = Str::slug($data['label'], '-');
+
+        // check if an image is given
+        if (Arr::exists($data, 'image')) {
+            // if exists an image, delete it to make space for the newest
+            if ($food->image) Storage::delete($food->image);
+            // todo take the image extension
+            // $extension = $data['image']->extension();
+            // todo build the image file name with the slug (unique causa depends on title witch is unique) + extension
+            // $file_name = "$food->slug.$extension";
+            // $img_url = Storage::putFileAs('foods', $data['image'], $file_name);
+            // define a variable where the file is saved in a path storage/app/public/{} that return a correct URL
+            $img_url = Storage::put('foods', $data['image']);
+            // change the file given with the correct url
+            $data['image'] = $img_url;
+        }
+
+        // define publish or not
+        $data['is_published'] = Arr::exists($data, 'is_published');
+
+        $food->update($data);
+
+        return to_route('admin.foods.index')->with('message', "$food->label updated succesfully.")->with('type', 'warning');
     }
 
     /**
