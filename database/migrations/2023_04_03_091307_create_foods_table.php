f064a592aa2347e48c62891c3e59cf24d3d47eee
DarioGiusino <dario.giusino@gmail.com>
2023-04-06 17:15:38 +0200

    made food label unique + added type and slug columns

--- database/migrations/2023_04_03_091307_create_foods_table.php
+++ database/migrations/2023_04_03_091307_create_foods_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('foods', function (Blueprint $table) {
             $table->id();
-            $table->string('label');
+            $table->string('label')->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 6, 2);
             $table->boolean('is_published')->default(0);
