b97e2d02b72b8a73a306e11297db1a78480c0347
DarioGiusino <dario.giusino@gmail.com>
2023-04-06 11:00:12 +0200

    category_restaurant seeder

---
+++ database/seeders/CategoryRestaurantSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Restaurant;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+
+class CategoryRestaurantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $category_ids = Category::pluck('id')->toArray();
+        $restaurant_ids = Restaurant::pluck('id')->toArray();
+
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('category_restaurant')->insert([
+                'category_id' => Arr::random($category_ids),
+                'restaurant_id' => Arr::random($restaurant_ids)
+            ]);
+        }
+    }
+}
