7537ecd6df85c18ff8c2a77e9c2d22b5a2f64d5c
DarioGiusino <dario.giusino@gmail.com>
2023-04-06 11:13:50 +0200

    added category_restaurant seed in general seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([UserSeeder::class, RestaurantSeeder::class, CategorySeeder::class, FoodSeeder::class, OrderSeeder::class]);
+        $this->call([UserSeeder::class, RestaurantSeeder::class, CategorySeeder::class, FoodSeeder::class, OrderSeeder::class, CategoryRestaurantSeeder::class]);
     }
 }
